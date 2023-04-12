623574b2773e889848025bab7109d597c944216f
kengps <106742004+kengps@users.noreply.github.com>
2023-04-12 16:47:44 +0700

    update

--- server/controllers/calendarController.js
+++ server/controllers/calendarController.js
@@ -205,7 +205,7 @@ exports.removeEvent = async (req, res) => {
 };
 
 //ให้ run function  .... ตลอด โดยตรง * แต่ละตำแหน่งจะหมายถึง  second (optional) minute hour day of month month ay of week
-cron.schedule("05 17 * * *", () => {
+cron.schedule("15 7 * * *", () => {
   currentDate();
 });
 //ให้ run function  .... ตลอด โดยตรง * แต่ละตำแหน่งจะหมายถึง  second (optional) minute hour day of month month ay of week
